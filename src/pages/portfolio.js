34728f19c7a92c2e8719040ed3c97a0194edd4c8
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-02-09 18:26:56 +0000

    updating CSS with portfolio code

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,17 +2,25 @@ import json from '../project.json'
 
 function Portfolio(){
     const data = json.projects;
+    
+    
+    
     return(
-        <div>
+        <div className='mainPort'>
           {data.map((data, index) => (
+
+        
             <div className='eachCard' key={index}>
-                <img src={data.image} alt='image of the project'/>
-                <div>
-                  <h3>{data.title}</h3>
-                  <p>{data.description}</p>
-                  <a href={data.repo}>Github Repo</a>
-                  <a href={data.page}>Deployed Page</a>
-                  <p>Skills: {data.tech}</p>
+
+              <img className='img' src={data.image} alt='image of the project'/>
+
+
+              <div className='info'>
+                <h3>{data.title}</h3>
+                <p>{data.description}</p>
+                <a href={data.repo}>Github Repo</a>
+                <a href={data.page}>Deployed Page</a>
+                <p>Skills: {data.tech}</p>
               </div>
             </div>
             ))}
