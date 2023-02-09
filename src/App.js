04f2a352540c1e11b18517b7ebca5a1b0ac4e193
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-02-09 22:36:02 +0000

    updated my headerjs with classname

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Header from "./components/header";
 import Footer from './components/footer';
-// import About from "./pages/about";
-// import Stack from './pages/stack';
-// import Contact from './pages/contact';
-// import Portfolio from './pages/portfolio';
+import About from "./pages/about";
+import Stack from './pages/stack';
+import Contact from './pages/contact';
+import Portfolio from './pages/portfolio';
 
 import { Routes, Route } from "react-router-dom";
 
