0f24d9db4f5759207747038eecf0ddce62f16def
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-02-11 11:32:22 +0000

    added fontawesome icon to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
 function Footer(){
     return (
         <footer>
+            <div className='icons'>
+                
+               <FontAwesomeIcon icon="fa-solid fa-envelope" />
+               <FontAwesomeIcon icon="fa-brands fa-github" />
+               <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+            </div>
            <p>© Ifeoma Ezeoke Company</p> 
         </footer>
     )
