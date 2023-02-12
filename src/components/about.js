60ed28810dcd291795a17813f195124439687111
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-02-12 18:50:28 +0000

    updated the portfoliojs to display pics, added about to components

---
+++ src/components/about.js
@@ -0,0 +1,44 @@
+import pic from "../images/mypic.jpg"
+
+function About() {
+  return (
+    <div className='about'>
+      <main>
+        <h1>Ifeoma Ezeoke</h1>
+        <h2>Frontend Developer</h2>
+      </main>
+      <div className='content'>
+        <div>
+          <img className='aboutImg' src={pic} alt="My profile pic" />
+        </div>
+
+        <div className='description'>
+          <p>
+
+            I am frontend developer who lives in London, United Kingdom.
+            I am passionate about my aquired skill and look forward to what the future holds in the industry.
+
+            So far, I have developed skills in the basics of frontend language as well as adding some frameworks and library to my stack.
+          </p>
+          <p>
+            I am a disciplined professional who is quick to learn and able to adapt to a variety of environment.
+
+            Since I discovered my passion for coding months ago, I have dedicated a lot of time to it and work hard daily to improve my proficiency.
+            I love what I do and I am willing to dedicate more time to learn and achieve my goal in the tech industry.
+          </p>
+        </div>
+
+      </div>
+
+
+
+    </div>
+
+
+  )
+
+
+}
+
+
+export default About;
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,33 +1,73 @@
 import json from '../project.json'
+import pic1 from './images/teamprofile.png'
+import pic2 from './images/readmefile.png'
+import pic3 from './images/weather-app.png'
+import pic4 from './images/day-planner-image.png'
+import pic5 from './images/passwordgenerator.png'
+import pic6 from './images/recipedia.png'
+
+
+
+
+
 
 function Portfolio(){
     const data = json.projects;
-    
-    
-    
+    const pics = [pic1, pic2, pic3, pic4, pic5, pic6];
+    const myCard = document.getElementsByClassName('eachCard');
+    const myPic = document.getElementsByClassName('img');
+  
+
     return(
-        <div className='mainPort'>
-          {data.map((data, index) => (
+      <div className='mainPort'>
+          <div className='eachCard'>
+              {data.map((data, index) => (
+              
+
+              
+             <div className='info' key={index}>
+                 <h3>{data.title}</h3>
+                 <p>{data.description}</p>
+                 <a target='_blank'  href={data.repo}>Github Repo</a>
+                 <a target='_blank' href={data.page}>Deployed Page</a>
+                 <p>Skills: {data.tech}</p>
+                 <img className='img' src={pics[index]} key={index} alt='image of the project'/>
+
+                 
+             </div>
+             
+              ))}
+            
+            
+           </div>
+      </div>
+    )
+    
+    
+    
+    // return(
+    //     <div className='mainPort'>
+    //       {data.map((data, index) => (
 
         
-            <div className='eachCard' key={index}>
+    //         <div className='eachCard' key={index}>
 
-              <img className='img' src={data.image} alt='image of the project'/>
+    //           <img className='img' src={data[6]} alt='image of the project'/>
 
 
-              <div className='info'>
-                <h3>{data.title}</h3>
-                <p>{data.description}</p>
-                <a target='_blank'  href={data.repo}>Github Repo</a>
-                <a target='_blank' href={data.page}>Deployed Page</a>
-                <p>Skills: {data.tech}</p>
-              </div>
-            </div>
-            ))}
+    //           <div className='info'>
+    //             <h3>{data.title}</h3>
+    //             <p>{data.description}</p>
+    //             <a target='_blank'  href={data.repo}>Github Repo</a>
+    //             <a target='_blank' href={data.page}>Deployed Page</a>
+    //             <p>Skills: {data.tech}</p>
+    //           </div>
+    //         </div>
+    //         ))}
           
-        </div>
+    //     </div>
           
-    )
+    // )
 }
 
 export default Portfolio;
