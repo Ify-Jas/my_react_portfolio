da647462e6243f9dec9f789af90970d9c7307ae1
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-07-17 21:45:24 +0100

    Browser routes added and routes fixed

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import Contact from "./pages/contact";
 import Header from "./component/header";
 import Footer from "./component/header";
 import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path={"/"} element={<Home />} ></Route>
+          <Route path={"/contact"} element={<Contact />} ></Route>
+        </Routes>
+
+
+        <Footer />
+      </BrowserRouter>
       
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} ></Route>
-        <Route path="/contact" element={<Contact />} ></Route>
-
-      </Routes>
-
-            
-      <Footer />
+     
      
 
 
