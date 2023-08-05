616cce6f34804503c6189c737ee0855f6eec5ca1
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-08-05 20:27:46 +0100

    css added for form page, logo updated

--- src/component/formPage.js
+++ src/component/formPage.js
@@ -1,12 +1,23 @@
 function FormPage(){
     return(
         <div>
-            <form>
-                <input type='text' placeholder="job, title"></input>
-                <input type='text' placeholder="location"></input>
-                <input type='range' placeholder="distance"></input>
-                <input type="number" placeholder="min"></input>
-                <input type="number" placeholder="max"></input>
+            <form className="form" >
+                <input type='text' placeholder="Job title"></input>
+                <input type='text' placeholder="Location"></input>
+                <select className="formDistance" placeholder="Distance">
+                    <option>Distance from Location</option>
+                    <option>1 mile</option>
+                    <option>3 miles</option>
+                    <option>5 miles</option>
+                    <option>10 miles</option>
+                    <option>15 miles</option>
+                    <option>20 miles</option>
+                    <option>30 miles</option>
+                    <option>50 miles</option>
+                </select>
+                <input type="number" placeholder="Min Salary £"></input>
+                <input type="number" placeholder="Max Salary £"></input>
+                <button type="button" className="formBtn">Search</button>
             </form>
         </div>
     )
--- src/component/header.js
+++ src/component/header.js
@@ -1,20 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../images/mylogo.png';
 
 function Header() {
   return (
     <>
 
-      <Navbar className="bg-body-tertiary">
+      <Navbar className="bg-body-tertiary header">
         <Container>
-          <Navbar.Brand href="#">
+          <Navbar.Brand href="#" className='brandName'>
             <img
-              alt=""
-              src="/img/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
+              alt="brand logo"
+              src={logo}
+              width="50"
+              height="50"
+              className="d-inline-block align-top logo"
+            />
             Jobs Inn
           </Navbar.Brand>
         </Container>
