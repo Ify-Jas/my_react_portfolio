d042982d1b225bba4a765c052bbad6b32051aceb
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-08-11 19:17:24 +0100

    updating with functionality on the form page

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,31 +1,43 @@
 function Footer(){
     return(
       <>
-        <div>
-            <h1>Jobs Inn</h1>
-            <div>
-                <a href="#">About Us</a>
-                <a href="#">Contact Us</a>
-                <a href="#">Privacy</a>
-                <a href="#">Terms and Condition</a>
-            </div>
+        <div className="footerContainer">
+                <div>
+                    <h1>Jobs Inn</h1>
+                    <div className="list">
+                        <a href="#">About Us</a>
+                        <a href="#">Contact Us</a>
+                        <a href="#">Privacy</a>
+                        <a href="#">Terms and Condition</a>
+                    </div>
+                </div>
+                <div>
+                    <h1>Social Media</h1>
+                    <div className="list">
+                        <a href="https://www.instagram.com/" target="_blank">Instagram</a>
+                        <a href="https://twitter.com/" target="_blank">Twitter</a>
+                        <a href="https://www.facebook.com/" target="_blank">Facebook</a>
+                    </div>
+                </div>
+                <div>
+                    <h1>Office Address</h1>
+                    <p>
+                        London Office: 
+                        <br />
+                        50 Bank Street, Canary wharf, London E14 5NR.
+                        <br />
+                        Telephone: 0208 800 5000
+                    </p>
+                    <p>New York Office:
+                    <br />
+                     12345 Downtown Street, NY 50102.
+                    <br />
+                     Telephone: +1(516) 234 56789
+                     </p>
+                </div>
+
         </div>
-        <div>
-            <h1>Social Media</h1>
-            <div>
-                <a href="#">About Us</a>
-                <a href="#">Contact Us</a>
-                <a href="#">Privacy</a>
-                <a href="#">Terms and Condition</a>
-            </div>
-        </div>
-        <div>
-            <h1>Office Address</h1>
-            <p>
-                London Office: 50 Bank Street, Canary wharf, London E14 5NR.
-            </p>
-            <p>New York Office: 12345 Downtown Street, NY 50102.</p>
-        </div>
+       
       </>
     )
 }
--- src/component/formPage.js
+++ src/component/formPage.js
@@ -1,10 +1,60 @@
+import {useState} from "react";
+
+const apiKey = '0996c468cae7cea3ab5d1ac0d4db8218';
+const appID = "a8f9fe6e";
+
+
+
+
+
+
+
+
 function FormPage(){
+    const [formData, setFormData] = useState({
+        title: '',
+        location: '',
+        distance: '',
+        minSalary: '',
+        maxSalary: ''    
+    })
+    
+    const handleChange = (event)=>{
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        })
+    }
+    const handleClick = ()=>{
+        if(!formData.title|| !formData.location){
+            return alert('Please add a job title and/or location')
+        } else{
+            fetch()
+        }
+    }
     return(
         <div>
             <form className="form" >
-                <input type='text' placeholder="Job title"></input>
-                <input type='text' placeholder="Location"></input>
-                <select className="formDistance" placeholder="Distance">
+                <input 
+                type='text' 
+                placeholder="Job title"
+                name='title'
+                onChange={handleChange}
+                ></input>
+
+                <input 
+                type='text' 
+                placeholder="Location"
+                name='location'
+                onChange={handleChange}
+                ></input>
+
+                <select 
+                className="formDistance" 
+                placeholder="Distance"
+                name='distance'
+                onChange={handleChange}
+                >
                     <option>Distance from Location</option>
                     <option>1 mile</option>
                     <option>3 miles</option>
@@ -15,9 +65,26 @@ function FormPage(){
                     <option>30 miles</option>
                     <option>50 miles</option>
                 </select>
-                <input type="number" placeholder="Min Salary £"></input>
-                <input type="number" placeholder="Max Salary £"></input>
-                <button type="button" className="formBtn">Search</button>
+
+                <input 
+                type="number" 
+                placeholder="Min Salary £"
+                name='minSalary'
+                onChange={handleChange}
+                ></input>
+
+                <input 
+                type="number" 
+                placeholder="Max Salary £"
+                name='maxSalary'
+                onChange={handleChange}
+                ></input>
+
+                <button 
+                type="button" 
+                className="formBtn"
+                onClick={handleClick}
+                >Search</button>
             </form>
         </div>
     )
