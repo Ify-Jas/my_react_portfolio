be6ad85987dd4580d31742c073e052553a84b3ca
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-08-13 15:22:21 +0100

    uploading original structure of result list with mapping

--- src/component/formPage.js
+++ src/component/formPage.js
@@ -2,6 +2,7 @@ import {useState} from "react";
 
 const apiKey = '0996c468cae7cea3ab5d1ac0d4db8218';
 const appID = "a8f9fe6e";
+const results = 40;
 
 
 
@@ -26,10 +27,37 @@ function FormPage(){
         })
     }
     const handleClick = ()=>{
-        if(!formData.title|| !formData.location){
-            return alert('Please add a job title and/or location')
+        if(!formData.title|| !formData.location || !formData.distance || !formData.minSalary || !formData.maxSalary){
+            return alert('Please complete all fields')
         } else{
-            fetch()
+            fetch(`http://api.adzuna.com:80/v1/api/jobs/gb/search/1?app_id=${appID}&app_key=${apiKey}&results_per_page=${results}&what=${formData.title}&where=${formData.location}&salary_min=${formData.minSalary}&salary_max=${formData.maxSalary}&distance=${formData.distance}&content-type=application/json`)
+
+            .then((res) => res.json())
+            .then((data)=>{
+                console.log(data)
+               setFormData({
+                title: '',
+                location: '',
+                distance: '',
+                minSalary: '',
+                maxSalary: ''  
+               })
+               {data.map((data, index)=>{
+                const result = document.getElementsByClassName(results);
+                result.append(
+                    <div>
+                        <h2>{data.title}</h2>
+                        <p>Company: {data.company.display_name}</p>
+                        <p>Location: {data.location.display_name}</p>
+                        <p>Salary: {data.salary_min}</p>
+                        <p>Contract time: {data.contract_time}</p>
+                        <p>Contract type: {data.contract_type}</p>
+                    </div>
+                )
+              
+             })}
+            })
+        
         }
     }
     return(
@@ -39,6 +67,7 @@ function FormPage(){
                 type='text' 
                 placeholder="Job title"
                 name='title'
+                value={formData.title}
                 onChange={handleChange}
                 ></input>
 
@@ -46,6 +75,7 @@ function FormPage(){
                 type='text' 
                 placeholder="Location"
                 name='location'
+                value={formData.location}
                 onChange={handleChange}
                 ></input>
 
@@ -53,23 +83,25 @@ function FormPage(){
                 className="formDistance" 
                 placeholder="Distance"
                 name='distance'
+                value={formData.distance}
                 onChange={handleChange}
                 >
-                    <option>Distance from Location</option>
-                    <option>1 mile</option>
-                    <option>3 miles</option>
-                    <option>5 miles</option>
-                    <option>10 miles</option>
-                    <option>15 miles</option>
-                    <option>20 miles</option>
-                    <option>30 miles</option>
-                    <option>50 miles</option>
+                    <option>Distance from Location(km)</option>
+                    <option>1 </option>
+                    <option>3 </option>
+                    <option>5 </option>
+                    <option>10</option>
+                    <option>15 </option>
+                    <option>20 </option>
+                    <option>30 </option>
+                    <option>50 </option>
                 </select>
 
                 <input 
                 type="number" 
                 placeholder="Min Salary £"
                 name='minSalary'
+                value={formData.minSalary}
                 onChange={handleChange}
                 ></input>
 
@@ -77,6 +109,7 @@ function FormPage(){
                 type="number" 
                 placeholder="Max Salary £"
                 name='maxSalary'
+                value={formData.maxSalary}
                 onChange={handleChange}
                 ></input>
 
@@ -86,6 +119,8 @@ function FormPage(){
                 onClick={handleClick}
                 >Search</button>
             </form>
+            <section className="result"> </section>
+                         
         </div>
     )
 
